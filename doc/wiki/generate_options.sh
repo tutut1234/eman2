339cb74538e0d64206bf144307edd03f08c80e58
shadow_walker <shadowwalkersb@gmail.com>
2018-04-07 22:54:30 -0500

    exclude programs [ci skip]

--- doc/wiki/generate_options.sh
+++ doc/wiki/generate_options.sh
@@ -3,6 +3,16 @@
 SRC_DIR="$(cd ../.. "$(dirname "$0")"; pwd -P)"
 progs=$( find "${SRC_DIR}/programs" -name 'e2*.py' -exec basename {} \; )
 
+progs_exclude=( e2.py e2projectmanager.py e2unwrap3d.py e2version.py e2fhstat.py e2_real.py 
+                e2seq2pdb.py        # uses OptionParser
+                e2proc3d_huge.py    # uses OptionParser
+                e2proc3d.py         # uses OptionParser
+)
+
+for f in ${progs_exclude[@]};do
+    progs=( ${progs[@]/$f} )
+done
+
 for f in ${progs[@]};do
     prog=${f%%.py}
     echo "Extracting from ${SRC_DIR}/programs/$f into $prog.txt"
