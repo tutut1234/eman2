e476012b8514edf9ff39ef3bd1aed2fef4545872
shadow_walker <shadowwalkersb@gmail.com>
2018-04-09 12:45:10 -0500

    run programs with --generate_doc [ci skip]

--- doc/wiki/generate_options.sh
+++ doc/wiki/generate_options.sh
@@ -6,5 +6,5 @@ progs=$( find "${SRC_DIR}/programs" -name 'e2*.py' -exec basename {} \; )
 for f in ${progs[@]};do
     prog=${f%%.py}
     echo "Extracting from ${SRC_DIR}/programs/$f into $prog.txt"
-    python "${SRC_DIR}"/examples/extracthelp.py "${SRC_DIR}"/programs/$f > ${prog}.txt
+    $f --generate_doc > ${prog}.txt
 done
