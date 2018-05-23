47f1da2ace877642f712abebacab25c823a38868
shadow_walker <shadowwalkersb@gmail.com>
2018-05-23 16:33:43 -0500

    mini-eman-deps-11.0

--- ci_support/setup_conda.sh
+++ ci_support/setup_conda.sh
@@ -4,6 +4,7 @@ set -xe
 
 # Download and install Miniconda
 export MINICONDA_URL="https://repo.continuum.io/miniconda"
+export MINICONDA_FILE="Miniconda2-4.4.10-Linux-x86_64.sh"
 
 curl -L -O "${MINICONDA_URL}/${MINICONDA_FILE}"
 bash $MINICONDA_FILE -b
@@ -12,5 +13,8 @@ bash $MINICONDA_FILE -b
 source ${HOME}/miniconda2/bin/activate root
 conda config --set show_channel_urls true
 
-conda update conda -c defaults --yes --quiet
-conda install "cmake=>3.9" -c defaults --yes --quiet
+conda update conda -c defaults --yes
+conda install conda-build -c defaults --yes
+conda install cmake=3.9 -c defaults --yes
+conda install eman-deps=11.0 -c cryoem -c defaults -c conda-forge --yes
+conda clean --all --yes
