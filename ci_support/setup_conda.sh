00679e8ca9e70ac1766b14f7e0589e9136251b10
shadow_walker <shadowwalkersb@gmail.com>
2018-05-24 09:59:26 -0500

    eman-deps=11.2

--- ci_support/setup_conda.sh
+++ ci_support/setup_conda.sh
@@ -5,5 +5,5 @@ set -xe
 # Configure conda
 source ${HOME}/miniconda2/bin/activate root
 
-conda install eman-deps=11.1 -c cryoem -c defaults -c conda-forge --yes
+conda install eman-deps=11.2 -c cryoem -c defaults -c conda-forge --yes
 conda clean --all --yes
