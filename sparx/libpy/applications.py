8141dc31820d79befd54c5c305765ae25379c84e
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2018-11-26 20:13:06 +0100

    Fixed indent problem in applications.py

--- sparx/libpy/applications.py
+++ sparx/libpy/applications.py
@@ -7979,9 +7979,8 @@ def mref_ali3d_MPI(stack, ref_vol, outdir, maskfile=None, focus = None, maxit=1,
 
 	if CTF:
 		if(data[0].get_attr_default("ctf_applied",0) > 0):  ERROR("mref_ali3d_MPI does not work for CTF-applied data", "mref_ali3d_MPI", 1, myid)
-		from reconstruction import rec3D_MPI
-	else:
-		from reconstruction import rec3D_MPI_noCTF
+	from reconstruction import rec3D_MPI
+	from reconstruction import rec3D_MPI_noCTF
 
 	if debug:
 		finfo.write( '%d loaded  \n' % len(data) )
