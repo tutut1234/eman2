d73a97ff1203d3353a39785a8c7e14e059d04ab5
Markus Stabrin <markus.stabrin@mpi-dortmund.mpg.de>
2018-12-20 04:31:49 +0100

    added sparx_legacy to sparx.py

--- sphire/libpy/sparx.py
+++ sphire/libpy/sparx.py
@@ -29,6 +29,7 @@ from __future__ import print_function
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
 #
 
+from sparx_legacy    import *
 from utilities       import *
 from filter          import *
 from projection      import *
