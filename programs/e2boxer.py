36c2235abe515d73749e691d23c1ddba4740fa33
shadow_walker <shadowwalkersb@gmail.com>
2019-01-25 16:30:53 -0600

    add topaz [ci skip]

--- programs/e2boxer.py
+++ programs/e2boxer.py
@@ -1151,6 +1151,30 @@ class boxerConvNet(QtCore.QObject):
 		return
 
 
+class boxerTopaz(QtCore.QObject):
+	
+	@staticmethod
+	def setup_gui(gridlay, boxerwindow=None):
+		boxerTopaz.boxerwindow=boxerwindow
+		boxerTopaz.bt_train=QtGui.QPushButton("Train")
+		boxerTopaz.bt_train.setToolTip("Topaz")
+		gridlay.addWidget(boxerTopaz.bt_train)
+		boxerTopaz.bt_train.clicked[bool].connect(boxerTopaz.do_training)
+		
+		boxerTopaz.threshold=ValSlider(None,(0,5.0),"Threshold",0.2,90)
+		gridlay.addWidget(boxerTopaz.threshold)
+		return
+	
+	@staticmethod
+	def do_training(args=None):
+		pass
+		
+	@staticmethod
+	def do_autobox(micrograph,goodrefs,badrefs,bgrefs,apix,nthreads,params,prog=None):
+		boxes = []
+		return boxes
+
+
 class boxerGauss(QtCore.QObject):
 	@staticmethod
 	def setup_gui(gridlay,boxerwindow=None):
@@ -1165,6 +1189,7 @@ aboxmodes = [ ("Local Search","auto_local",boxerLocal),
 	     ("by Ref","auto_ref",boxerByRef), 
 #	     ("Gauss","auto_gauss",boxerGauss),
 	     ("NeuralNet", "auto_convnet", boxerConvNet),
+	     ("Topaz", "auto_topaz", boxerTopaz),
 			  ]
 boxcolors = { "selected":(0.9,0.9,0.9), 
 			  "manual":(0,0,0.7), 
@@ -1176,6 +1201,7 @@ boxcolors = { "selected":(0.9,0.9,0.9),
 			  "auto_ref":(.1,.3,.4), 
 			  "auto_gauss":(.4,.1,.4), 
 			  "auto_convnet":(.1,.5,.1),
+			  "auto_topaz":(.5,.1,.5),
 			}
 
 class GUIBoxer(QtGui.QWidget):
