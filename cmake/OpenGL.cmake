ed6c0439a96dd7a9098c1ab05be1bbf389944aa8
shadow_walker <shadowwalkersb@gmail.com>
2019-04-09 22:32:22 -0500

    imported targets

--- cmake/OpenGL.cmake
+++ cmake/OpenGL.cmake
@@ -21,13 +21,10 @@ endif()
 message_var(OPENGL_INCLUDE_DIR)
 message_var(OPENGL_LIBRARIES)
 
-if(OpenGL_FOUND AND NOT TARGET OpenGL)
-	add_library(OpenGL INTERFACE)
-	add_library(OpenGL::OpenGL ALIAS OpenGL)
-	set_target_properties(OpenGL PROPERTIES
+if(OpenGL_FOUND)
+	set_target_properties(OpenGL::GL PROPERTIES
 						  INTERFACE_COMPILE_DEFINITIONS USE_OPENGL
 						  )
-	target_link_libraries(OpenGL INTERFACE OpenGL::GL OpenGL::GLU)
 		
 	# Symlink to GL. When the value is /usr/include, cmake ignores it.
 	# So, this is a workaround to include OpenGL headers
